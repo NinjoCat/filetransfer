464588674772bb6eedd426aadcf24a89651645a0
Ninja <esomina@gmail.com>
2017-02-02 21:11:28 +0300

    1

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ $factory = new Factory();
 
 try
 {
-    $conn = $factory->getConnection('ssh', 'kate', 'sE1RGn1w', '54.149.133.230', 22);
+    $conn = $factory->getConnection('ssh', 'kate', '', '', 22);
     var_dump($conn->pwd());
     $conn->download('/home/kate/1.txt', '/home/dev/1.txt');
     $conn->upload('/home/dev/111.txt');
